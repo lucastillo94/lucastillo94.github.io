8cadf053384bc5419ff6369d6375e14a9a3d3ce5
lucastillo94 <102324239+lucastillo94@users.noreply.github.com>
2023-02-28 23:52:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+document.getElementById('boton_ocultar1').addEventListener('click', function() {
+    document.getElementById('demo1').style.display = 'none';
+});
+
+document.getElementById('boton_mostrar1').addEventListener('click', function() {
+    document.getElementById('demo1').style.display = 'block';
+});
+
+document.getElementById('boton_ocultar2').addEventListener('click', function() {
+    document.getElementById('demo2').style.display = 'none';
+});
+
+document.getElementById('boton_mostrar2').addEventListener('click', function() {
+    document.getElementById('demo2').style.display = 'block';
+});
+
+document.getElementById('boton_ocultar3').addEventListener('click', function() {
+    document.getElementById('demo3').style.display = 'none';
+});
+
+document.getElementById('boton_mostrar3').addEventListener('click', function() {
+    document.getElementById('demo3').style.display = 'block';
+});
